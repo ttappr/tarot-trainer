4825ddf42552740bd7a78fea01a83099c5993a34
todd <jupitertitan05@gmail.com>
2020-11-22 17:18:34 -0800

    Switched to using a simple .js file to hold the card data.

--- www/js/flash-card.js
+++ www/js/flash-card.js
@@ -15,6 +15,8 @@ class FlashCard extends HTMLElement {
                 _reverse : this.getAttribute('reverse')  || reverse
             });
 
+        console.info(CARD_DATA);
+
         this.attachShadow({mode: 'open'});
 
         let image = new Image();
@@ -24,85 +26,26 @@ class FlashCard extends HTMLElement {
         this.shadowRoot.append(image);
         this.shadowRoot.append(new Text(this.getAttribute("question")));
     }
+    connectedCallback() {
+        super.connectedCallback();
+    }
+    disconnectedCallback() {
+        super.disconnectedCallback();
+    }
+    attributeChangedCallback(name, oldValue, newValue) {
+        super.attributeChangedCallback(name, oldValue, newValue);
+    }
+    adoptedCallback() {
+        super.adoptedCallback();
+    }
 }
 
 class FlashCardDeck extends HTMLElement {
     constructor(dataURL) {
         super();
-        dataURL = dataURL || this.getAttribute('data') || "./js/card-data.json";
-        this._cards = [];
         this.attachShadow({mode: 'open'});
-
-        document.addEventListener("deviceready", () => {
-            console.info("Trying to read JSON file...");
-            this.loadJSON(dataURL).then((data)=> {
-                for (let obj of data) {
-                    obj.pic = "./img/" + obj.pic;
-                    this._cards.push(new FlashCard(...Object.values(obj)));
-                }
-                console.info(this._cards);
-            }).catch((err) => {
-                console.error(`loadJSON(${dataURL}) ERROR: ${err}`);
-            });
-        }, false);
-    }
-
-    _reqFS() {
-        return new Promise((resolve, reject) => {
-            window.requestFileSystem(
-                LocalFileSystem.PERSISTENT, 0, 
-                (fs) => {
-                    return resolve(fs);
-                }, (err) => {
-                    return reject(err);
-                });
-        });
-    }
-    
-    _getFile(fs, path) {
-        return new Promise((resolve, reject) => {
-            fs.root.getFile(
-                path, {create: false, exclusive: true},
-                (fileEntry) => {
-                    return resolve(fileEntry);
-                }, (err) => {
-                    return reject(err);
-                });
-        });
-    }
-    _getText(fileEntry) {
-        return new Promise((resolve, reject) => {
-            fileEntry.file((file) => {
-                let reader = new FileReader();
-                reader.onloaded = () => {
-                    return resolve(this.result);
-                };
-                reader.onerror = () => {
-                    return reject(this.error);
-                };
-                reader.readAsText(file);
-            }, (err) => {
-                return reject(err);
-            });
-        });
-    }
-    _resolveLocalFSUrl(url) {
-        return new Promise((resolve, reject) => {
-            window.resolveLocalFileSystemURL(url, 
-                (fs) => {
-                    return resolve(fs);
-                }, (err) => {
-                    return reject(err);
-                });
-        });
-    }
-
-    async loadJSON(path) {
-        let appBase     = cordova.file.applicationDirectory + "www/";
-        let fs          = await this._resolveLocalFSUrl(appBase);
-        let fileEntry   = await this._getFile(fs, path);
-        let text        = await this._getText(fileEntry);
-        return JSON.parse(text);
+        dataURL = dataURL || this.getAttribute('data');
+        console.info(CARD_DATA)
     }
 }
 
