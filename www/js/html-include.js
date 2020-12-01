9b8ef8d4a22fc52cd15aaef664bd1416d4a23345
todd <jupitertitan05@gmail.com>
2020-11-30 18:44:04 -0800

    Experimental error handling code for html-include

--- www/js/html-include.js
+++ www/js/html-include.js
@@ -1,22 +1,37 @@
 
+/**
+ * Custom element <html-include> to operate like a C #include. The URL to the
+ * HTML file to be included must be placed between the open and close tags of
+ * this element: <html-include>./partials/my-partial-page.html</html-include>.
+ */
 class HTMLInclude extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: "open"});
+        this._populated = false;
     }
-    /**
-     * Called every time the element is inserted into the DOM. 
-     */
     connectedCallback() {
-        setTimeout(() => this._fetch());
+        //setTimeout(() => this._fetch());
+        this._fetch();
     }
     async _fetch() {
-        let src = this.textContent;
-        let rsp = await fetch(src);
-        let txt = await rsp.text();
-        this.shadowRoot.innerHTML = txt;
+        try {
+            if (this.isConnected && !this._populated) {
+                this._populated = true;
+                let src = this.textContent;
+                let rsp = await fetch(src);
+                let txt = await rsp.text();
+                this.shadowRoot.innerHTML = txt;
+            }
+        } catch (error) {
+            console.error(`HTMLInclude._fetch() caught an error: ${error}`);
+            this._populated = false;
+            throw error;
+        }
     }
 }
-
-// Register the custom element.
 customElements.define("html-include", HTMLInclude);
+
+async function foo() {
+    console.warn(`foo() called`);
+}
