c9e087ec50efd466c3f3cb89421138aea9ce4b57
ttappr <tmtappr@gmail.com>
2020-12-12 15:33:18 -0800

    Removed dependency on long-press-event.js.

--- www/js/tool-tip.js
+++ www/js/tool-tip.js
@@ -4,7 +4,8 @@ class ToolTip extends HTMLElement {
     constructor(value) {
         super();
         this.attachShadow({mode: 'open'});
-        this._parent = this.shadowRoot.parentElement;
+        this._pressTimer = null;
+        this._timeout = 1000;
         this._load();
     }
     async _load() {
@@ -14,63 +15,39 @@ class ToolTip extends HTMLElement {
         this.textContent  = "";
         let root          = this.shadowRoot;
         root.innerHTML    = html;
-        this._tip_content = root.querySelector(".tip-content");
+        let tc            = root.querySelector(".tip-content");
+        this._tip_content = tc;
+        tc.innerHTML      = tip;
+        let target        = this.getAttribute("target") 
 
-        if (this.hasAttribute("target")) {
-            var parent = document.querySelector(this.getAttribute("target"));
-        } else {
-            var parent = this.parentElement;
-        }
+        target = (target) ? document.querySelector(target) : this.parentElement;
 
-        this._tip_content.innerHTML = tip;
-        this._tip_content.addEventListener("click", 
-                                           this._onTipClick.bind(this));
-        parent.addEventListener("long-press", this._onQueryTip.bind(this));
-        parent.addEventListener("contextmenu", this._onQueryTip.bind(this));
+        target.addEventListener("mousedown",   this._startTimer.bind(this));
+        target.addEventListener("mouseup",     this._cancelTimer.bind(this));
+        target.addEventListener("mousemove",   this._cancelTimer.bind(this))
+        target.addEventListener("contextmenu", this._showTip.bind(this));
+
+        tc.addEventListener("click", this._dismissTip.bind(this));
     }
-    _onQueryTip(e) {
-        this._tip_content.style.display = "block";
-        if (e.detail) {
-            this._tip_content.style.left = `${e.detail.clientX}px`;
-            this._tip_content.style.top = `${e.detail.clientY}px`;
-        } else {
-            this._tip_content.style.left = `${e.clientX}px`;
-            this._tip_content.style.top = `${e.clientY}px`;
-        }
-        this._tip_content.style["z-index"] = 99;
+    _startTimer(e) {
+        this._pressTimer = setTimeout(this._showTip.bind(this), 
+                                      this._timeout, e);
         e.preventDefault();
         return false;
     }
-    _onTipClick(e) {
+    _cancelTimer(e) {
+        clearTimeout(this._pressTimer);
+    }
+    _showTip(e) {
+        e.preventDefault();
+        let tc = this._tip_content;
+        tc.style.left = e.clientX + 'px';
+        tc.style.top = e.clientY + 'px';
+        tc.style["z-index"] = 99;
+        tc.style.display = "block";
+    }
+    _dismissTip(e) {
         this._tip_content.style.display = "none";
-    }
-    /**
-     * The attributes this element is interested in.
-     * @returns {string[]} The names of the attributes to watch for.
-     */
-    static get observedAttributes() {
-    }
-    /**
-     * Called when an observed attribute has been added, removed, updated, or 
-     * replaced. 
-     * @param {string} name The name of the attribute.
-     * @param {string} oldValue The old value of the attribute.
-     * @param {string} newValue The new value of the attribute.
-     */
-    attributeChangedCallback(name, oldValue, newValue) {
-    }
-    /**
-     * Called every time the element is inserted into the DOM. 
-     */
-    connectedCallback() {
-        // Useful for  running setup code, such as fetching resources or 
-        // rendering. Generally,  you should try to delay work until this time.
-    }
-    /**
-     * Called when element has been moved into a new document.
-     */
-    adoptedCallback() {
-
     }
 }
 
